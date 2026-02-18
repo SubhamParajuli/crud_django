d3facff16725a6956439154b193fa4730f0f2f46
Subham Parajuli <suvamparajuli222@gmail.com>
2026-02-18 11:39:56 +0545

    formatiing input and input from html forms

--- core/receipe_bud/views.py
+++ core/receipe_bud/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
+
+def receipe_page(request):
+    if request.method=="POST":
+        data=request.POST
+        receipe_image=request.FILES.get('receipe_image')
+        receipe_name=data.get('receipe_name')
+        receipe_description=data.get('receipe_description')
+
+        Receipe.objects.create(
+            receipe_image=receipe_image,
+            receipe_name=receipe_name,
+            receipe_description=receipe_description)
+        return redirect('/')
+    
+    queryset=Receipe.objects.all()
+    context={
+        'receipes':queryset
+    }   
+
+    return render(request,'receipe.html',context)
