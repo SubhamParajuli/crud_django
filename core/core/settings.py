150faa2028c181b4351970e7c01975cf2ae7070e
Subham Parajuli <suvamparajuli222@gmail.com>
2026-02-19 15:49:47 +0545

    Update ALLOWED_HOSTS and CSRF settings
    Restrict allowed hosts for production settings and add CSRF trusted origins.

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,10 +25,15 @@ SECRET_KEY = 'django-insecure-+o4b7xt3mlx9vi9r)5ur5hf#_o2$wzd2i^fb_b)+_+*1sib7g2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "crud-django-c606.onrender.com",
-    "*"
+
+
+ALLOWED_HOSTS = ["crud-django-c606.onrender.com"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://crud-django-c606.onrender.com",
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
